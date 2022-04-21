8fb59dfb2c24892553590841b69b8be42e9ef0ec
Jeevan Bastola <jeevanbastola986@gmail.com>
2022-04-21 18:15:31 -0400

    navbar working...

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Home from './screens/Home';
+import PlansPricing from './screens/PlansPricing';
+import Facilities from './screens/Facilities';
+import Reserve from './screens/Reserve';
 import Blog from './screens/Blog';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      {/* Header*/}
-      <Header/>
-      {/* App Body */}
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Header*/}
+        <Header/>
+        <div className="content">
+          {/* App Body */}
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/home" element={<Home/>}/>
+            <Route exact path="/plans&pricing" element={<PlansPricing/>}/>
+            <Route exact path="/reserve" element={<Reserve/>}/>
+            <Route exact path="/blog" element={<Blog/>}/>
+            <Route exact path="/facilities" element={<Facilities/>}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Header.css'
 import logo from './logo.svg';
+import ping_pong from './ping_pong.png';
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import BeenhereIcon from '@mui/icons-material/Beenhere';
 import BookIcon from '@mui/icons-material/Book';
 import DeckIcon from '@mui/icons-material/Deck';
+import {Link} from 'react-router-dom';
 
 function Header(){
     return(
@@ -16,11 +18,11 @@ function Header(){
                 <img src={logo} className="App-logo" />
             </div>
             <div className= "header_right">
-                <HeaderOption Icon={HomeIcon} title="Home"/>
-                <HeaderOption Icon={AttachMoneyIcon} title="Plans & Pricing"/>
-                <HeaderOption Icon={BeenhereIcon} title="Reserve"/>
-                <HeaderOption Icon={BookIcon} title="Blog"/>
-                <HeaderOption Icon={DeckIcon} title="Facilities"/>
+                <Link to="/home"><HeaderOption Icon={HomeIcon} title="Home"/></Link>
+                <Link to="/plans&pricing"><HeaderOption Icon={AttachMoneyIcon} title="Plans & Pricing"/></Link>
+                <Link to="/reserve"><HeaderOption Icon={BeenhereIcon} title="Reserve"/></Link>
+                <Link to="/blog"><HeaderOption Icon={BookIcon} title="Blog"/></Link>
+                <Link to="/facilities"><HeaderOption Icon={DeckIcon} title="Facilities"/></Link>
             </div>
         </div>
     );
