edd32dfa78478913321ab0b639ad9ea0a44d5691
shahidemdad <40446622+shahidemdad@users.noreply.github.com>
2022-04-22 06:17:24 -0400

    updated plans&pricing page

--- src/screens/PlansPricing.js
+++ src/screens/PlansPricing.js
@@ -1,30 +1,75 @@
 import React from "react";
-import './PlansPricing.css'
+import './PlansPricing.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from "react-bootstrap";
 
-export default function PlansPricing(){
-    return(
-        <><h1>
-            Plans and pricing Screen
-        </h1>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm">
-                    <h2>
-                        Beginner
-                    </h2>
-                    <h2 class="money">
-                        $15
-                    </h2>
-                    
-                </div>
-                <div class="col-sm">
-                    One of three columns
-                </div>
-                <div class="col-sm">
-                    One of three columns
-                </div>
-            </div>
-        </div> 
-        </>
+export default function PlansPricing() {
+    return (
+        <div className="PlansPricing Page">
+            <h1 class="pricing-title">
+                Choose your pricing plan
+            </h1>
+
+            <Container class="wrapper">
+                <Row>
+                    <Col>
+                        <div class="up-box">
+                            <h3>Beginner</h3>
+                            <h1>$15</h1>
+                            <p><br></br>
+                                Explore the basics with this <br></br>introductory course</p>
+                            <h6>Valid of 3 months</h6>
+                            <button>Select</button>
+                        </div>
+                        <div class="down-box">
+                            <ul>6 classes</ul>
+                            <ul>3 individual sessions</ul>
+                            <ul>Online resources</ul>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div class="up-box">
+                            <h3>Pro</h3>
+                            <h1>$10</h1>
+                            <p>Every month <br></br>
+                                Perfect for creatives, freelancers, and <br></br>
+                                experienced
+                            </p>
+                            <h6>Valid of 12 months</h6>
+                            <button>Select</button>
+                        </div>
+                        <div class="down-box">
+                            <ul>6 classes</ul>
+                            <ul>3 individual sessions</ul>
+                            <ul>Online resources</ul>
+                            <ul>1 guest pass</ul>
+                            <ul>Free WiFi</ul>
+                            <ul>Weekly newsletter</ul>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div class="up-box">
+                            <h3>VIP</h3>
+                            <h1>$10</h1>
+                            <p>Every month <br></br>
+                                Unlimited access to professional
+                                <br></br>content
+                            </p>
+                            <h6>Valid of 12 months</h6>
+                            <button>Select</button>
+                        </div>
+                        <div class="down-box">
+                            <ul>6 classes</ul>
+                            <ul>3 individual sessions</ul>
+                            <ul>Online resources</ul>
+                            <ul>1 guest pass</ul>
+                            <ul>Free WiFi</ul>
+                            <ul>Weekly newsletter</ul>
+                            <ul>Priority Support</ul>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
