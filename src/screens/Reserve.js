6f736649778d75d123a67254e5dcc6edf9ff72c2
ander9991 <sgranat000@citymail.cuny.edu>
2022-04-21 22:25:32 -0400

    Fixed Reserve + Bootstrap

--- src/screens/Reserve.js
+++ src/screens/Reserve.js
@@ -4,22 +4,25 @@ import "./Reserve.css"
 export default function Reserve(){
     return(
         <div className="wrapper">
-            <Card 
+            <Card
             img="https://images.unsplash.com/photo-1609710228159-0fa9bd7c0827?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGluZyUyMHBvbmd8ZW58MHx8MHx8&w=1000&q=80"
             title="Private Lesson"
             desc="$100 per hour"
+            x = "1"
             />
 
-            <Card 
+            <Card
             img="https://edqkvt6c5r7.exactdn.com/wp-content/uploads/2021/01/Ballers-Clubhouse-Ping-Pong-Precinct-2.jpg?strip=all&lossy=1&ssl=1"
             title="Clubhouse"
             desc="$15 per hour"
+            x = "2"
             />
 
-            <Card 
+            <Card
             img="https://awrwebsauksprod.blob.core.windows.net/assets/media/filer_public_thumbnails/filer_public/ae/77/ae77dd12-01c7-45b8-af32-0db01426175c/mlr-food-drink-22.jpg__652x652_q85_crop_subsampling-2_upscale.jpg"
             title="Food & Drinks"
             desc="$10"
+            x = "3"
             />
         </div>
 
@@ -34,7 +37,7 @@ function Card(props) {
                 <h2 className="card_title">{props.title}</h2>
                 <p className="card_desc">{props.desc}</p>
             </div>
-            <button className="card_btn">Reserve</button>
+            <button className="card_btn" >Reserve</button>
         </div>
     )
 }
