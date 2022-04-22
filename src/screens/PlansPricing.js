c317ad23e8ff9586cebb939dd0d337ebb7ff7a7b
shahidemdad <shahidbinemdad@gmail.com>
2022-04-21 22:35:58 -0400

    added file

--- src/screens/PlansPricing.js
+++ src/screens/PlansPricing.js
@@ -1,9 +1,32 @@
 import React from "react";
+import './PlansPricing.css'
 
 export default function PlansPricing(){
     return(
-        <h1>
+        <><h1>
             Plans and pricing Screen
         </h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <h2>
+                        Beginner
+                    </h2>
+                    <h2 class="money">
+                        $15
+                    </h2>
+                    
+                </div>
+                <div class="col-sm">
+                    One of three columns
+                </div>
+                <div class="col-sm">
+                    One of three columns
+                </div>
+            </div>
+        </div>
+            
+        </>
+    
     );
 }
