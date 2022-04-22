f28a141a23118a4481c0f773273d756c78008cdd
Jeevan Bastola <jeevanbastola986@gmail.com>
2022-04-22 18:13:00 -0400

    Home Screen Complete

---
+++ src/screens/Form.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {useState} from 'react';
+import './Form.css'
+function Form() {
+    const [name, setName]=useState('');
+    const [phone, setPhone]=useState("");
+    const [email, setEmail]=useState('');
+    const [subject, setSubject]=useState('');
+    const [message, setMessage]=useState('');
+    const [isPending, setIsPending]= useState(false);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const contact_message = {name, phone, email,subject, message};
+        console.log(contact_message)
+        
+        setIsPending(true);
+
+        console.log("the message has been submitted!");
+
+        setIsPending(false)
+
+        setName("");
+        setPhone("");
+        setEmail("");
+        setSubject("");
+        setMessage("");
+    }
+
+    return (
+        <div className='contact_us_form'>
+            <form onSubmit={handleSubmit}>
+                <label>Name: </label>
+                <input type="text" required value={name} onChange={(e)=> setName(e.target.value)}/>
+                <label>Phone: </label>
+                <input type="text" value={phone} onChange={(e)=> setPhone(e.target.value)}/>
+                <label>Email: </label>
+                <input type="text" required value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                <label>Subject: </label>
+                <input type="text" required value={subject} onChange={(e)=> setSubject(e.target.value)}/>
+                <label>Message: </label>
+                <textarea required rows="6" value={message} onChange={(e)=> setMessage(e.target.value)}></textarea>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Form
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,15 +2,25 @@ import React from "react";
 import './Home.css';
 import home_top from '../images/home_top.png'
 import home_two from '../images/home_two.png'
+import home_three from '../images/home_three.png'
+import home_four from '../images/home_four.png'
+import Form from "./Form";
 
 export default function Home(){
+    const handleScroll = () =>{
+        window.scroll({
+            top:document.body.offsetHeight,
+            left:0,
+            behavior: 'smooth',
+        })
+    }
     return(
         <div className="home_body">
             <div className='top_image_row'>
                 <img src={home_top} className="welcome" />
                 <div className='top_image_text'>
                     <div className='overlay'>Brooklyn Table Tennis Club</div>
-                    <button className='contact_button'> Contact </button>
+                    <button className='contact_button' onClick={handleScroll}> Contact </button>
                 </div>
             </div>
             <div className='about_us'>
@@ -25,6 +35,40 @@ export default function Home(){
                         Tennis Club.</p>
                 </div>
                 <img src={home_two} className="about_us_image" />
+            </div>
+            <div className="amenities">
+                <h1>Amenities</h1>
+                <h4>EVERYTHING YOU NEED</h4>
+            </div>
+            <div className='about_us'>
+                <div className='about_us_text'>
+                    <h1>Competetive Pricing</h1>
+                    <p>After a hard day at work, there’s no better way to train than letting go of all 
+                        your worries at our state-of-the-art Sports & Fitness Club. Our Competitive Pricing 
+                        has all the necessary accompaniments to make your experience as enjoyable as can be. 
+                        For more information about staying fit and active with us, please contact us below.</p>
+                </div>
+                <img src={home_three} className="about_us_image" />
+            </div>
+            <div className='about_us'>
+                <img src={home_four} className="about_us_image" />
+                <div className='about_us_text'>
+                    <h1>1 Year Warranty</h1>
+                    <p>Our newly renovated 1 Year Warranty is fully equipped with the latest amenities. 
+                        More than just a place for sports, at Brooklyn Table Tennis Club, we’re here to offer 
+                        you great experiences every step of the way. Stop by for a tour, and see what we can offer 
+                        you.</p>
+                </div>
+            </div>
+            <div className="contact_us">
+                <div className="contact_us_text">
+                    <h1>Contact Us</h1>
+                    <p>1100 Coney Island Ave #8, Brooklyn, NY 11230, USA</p>
+                    <p>(718) 421-2200</p>
+                </div>
+                <div className="contact_us_form">
+                    <Form/>
+                </div>
             </div>
         </div>
     );
