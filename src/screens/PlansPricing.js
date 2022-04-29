51ac3396e456b20ad7e0fb6731cb11df5938ee69
ander9991 <sgranat000@citymail.cuny.edu>
2022-04-28 20:54:18 -0400

    Plans and pricing page

--- src/screens/PlansPricing.js
+++ src/screens/PlansPricing.js
@@ -1,75 +1,115 @@
 import React from "react";
 import './PlansPricing.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from "react-bootstrap";
 
 export default function PlansPricing() {
-    return (
-        <div className="PlansPricing Page">
-            <h1 class="pricing-title">
-                Choose your pricing plan
-            </h1>
+    const listItems1 = ["6 classes"]
+    const listItems2 = ["3 individaul sessions"]
+    const listItems3 = ["Online resources"]
+    const listItems4 = [ "1 Guest Pass"]
+    const listItems5 = [ "Free WiFi"]
+    const listItems6 = [ "Weekly newsletter"]
+    const listItems7 = ["Priority Support"]
+    return(
+        <div className="wrapper">
+            <Card3
+            title = "Beginner"
+            h1= "$15"
+            para = "Explore the basiscs with this introductory courses"
+            i1={listItems1}
+            i2={listItems2}
+            i3={listItems3}
+            />
 
-            <Container class="wrapper">
-                <Row>
-                    <Col>
-                        <div class="up-box">
-                            <h3>Beginner</h3>
-                            <h1>$15</h1>
-                            <p><br></br>
-                                Explore the basics with this <br></br>introductory course</p>
-                            <h6>Valid of 3 months</h6>
-                            <button>Select</button>
-                        </div>
-                        <div class="down-box">
-                            <ul>6 classes</ul>
-                            <ul>3 individual sessions</ul>
-                            <ul>Online resources</ul>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div class="up-box">
-                            <h3>Pro</h3>
-                            <h1>$10</h1>
-                            <p>Every month <br></br>
-                                Perfect for creatives, freelancers, and <br></br>
-                                experienced
-                            </p>
-                            <h6>Valid of 12 months</h6>
-                            <button>Select</button>
-                        </div>
-                        <div class="down-box">
-                            <ul>6 classes</ul>
-                            <ul>3 individual sessions</ul>
-                            <ul>Online resources</ul>
-                            <ul>1 guest pass</ul>
-                            <ul>Free WiFi</ul>
-                            <ul>Weekly newsletter</ul>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div class="up-box">
-                            <h3>VIP</h3>
-                            <h1>$10</h1>
-                            <p>Every month <br></br>
-                                Unlimited access to professional
-                                <br></br>content
-                            </p>
-                            <h6>Valid of 12 months</h6>
-                            <button>Select</button>
-                        </div>
-                        <div class="down-box">
-                            <ul>6 classes</ul>
-                            <ul>3 individual sessions</ul>
-                            <ul>Online resources</ul>
-                            <ul>1 guest pass</ul>
-                            <ul>Free WiFi</ul>
-                            <ul>Weekly newsletter</ul>
-                            <ul>Priority Support</ul>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+            <Card6
+            title = "Pro"
+            h1= "$10"
+            para = "Every month Perfect for creatives, freelancers, and experienced"
+            i1={listItems1}
+            i2={listItems2}
+            i3={listItems3}
+            i4={listItems4}
+            i5={listItems5}
+            i6={listItems6}
+            />
+
+            <Card7
+            title="VIP"
+            h1="$10"
+            para="Every month Unlimited access to professional"
+            i1={listItems1}
+            i2={listItems2}
+            i3={listItems3}
+            i4={listItems4}
+            i5={listItems5}
+            i6={listItems6}
+            i7={listItems7}
+            />
         </div>
+
     );
+}
+
+function Card3(props) {
+    return (
+        <div className="card">
+            <div className="card_body">
+                <h2 className="card_title">{props.title}</h2>
+                <h1 className="card_h1"> {props.h1} </h1>
+                <br></br>
+                <p> {props.para} </p>
+            </div>
+            <button className="card_btn" >Select</button>
+            <br>
+            </br>
+            <li className="card_listItems"> {props.i1} </li>
+            <li className="card_listItems"> {props.i2} </li>
+            <li className="card_listItems"> {props.i3} </li>
+        </div>
+    )
+}
+
+function Card6(props) {
+    return (
+        <div className="card">
+            <div className="card_body">
+                <h2 className="card_title">{props.title}</h2>
+                <h1 className="card_h1"> {props.h1} </h1>
+                <br></br>
+                <p> {props.para} </p>
+            </div>
+            <button className="card_btn" >Select</button>
+            <br>
+            </br>
+            <li className="card_listItems"> {props.i1} </li>
+            <li className="card_listItems"> {props.i2} </li>
+            <li className="card_listItems"> {props.i3} </li>
+            <li className="card_listItems"> {props.i4} </li>
+            <li className="card_listItems"> {props.i5} </li>
+            <li className="card_listItems"> {props.i6} </li>
+        </div>
+    )
+}
+
+function Card7(props) {
+    return (
+        <div className="card">
+            <div className="card_body">
+                <h2 className="card_title">{props.title}</h2>
+                <h1 className="card_h1"> {props.h1} </h1>
+                <br></br>
+                <p> {props.para} </p>
+            </div>
+            <button className="card_btn" >Select</button>
+            <br>
+            </br>
+            <li className="card_listItems"> {props.i1} </li>
+            <li className="card_listItems"> {props.i2} </li>
+            <li className="card_listItems"> {props.i3} </li>
+            <li className="card_listItems"> {props.i4} </li>
+            <li className="card_listItems"> {props.i5} </li>
+            <li className="card_listItems"> {props.i6} </li>
+            <li className="card_listItems"> {props.i7} </li>
+        </div>
+    )
 }
